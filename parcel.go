6c40543e561c7e30654110d964ecaa9b889b29d1
Panfilov Denis <panfilov.denis96@gmail.com>
2025-07-22 02:38:59 +0300

    feat(parcel): реализован метод ParcelStore.Add

--- parcel.go
+++ parcel.go
@@ -14,9 +14,18 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
+	res, err := s.db.Exec("INSERT INTO parcel (client, status, address, createdAt) VALUES (:client, :status, :address, :createdAt)",
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("createdAt", p.CreatedAt))
+	if err != nil {
+		return 0, err
+	}
 
-	// верните идентификатор последней добавленной записи
-	return 0, nil
+	id, _ := res.LastInsertId()
+
+	return int(id), nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {

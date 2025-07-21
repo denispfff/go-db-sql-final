b9a30b55b7ec15d047404179f69f559760442dc5
Panfilov Denis <panfilov.denis96@gmail.com>
2025-07-22 02:27:27 +0300

    feat(main): мелкие правки в main на основании комментов

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"time"
 
 	_ "modernc.org/sqlite"
@@ -98,6 +99,10 @@ func (s ParcelService) Delete(number int) error {
 
 func main() {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		log.Error(err)
+	}
 
 	store := // создайте объект ParcelStore функцией NewParcelStore
 	service := NewParcelService(store)

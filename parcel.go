6e4221f0593271070066cdab3ccb33516a6bdb75
Panfilov Denis <panfilov.denis96@gmail.com>
2025-07-24 03:11:34 +0300

    feat(parcel): реализованы методы ParselStore.SetAddress и ParselStore.Delete

--- parcel.go
+++ parcel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type ParcelStore struct {
@@ -29,34 +30,86 @@ func (s ParcelStore) Add(p Parcel) (int, error) {
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
-	// реализуйте чтение строки по заданному number
-	// здесь из таблицы должна вернуться только одна строка
+	row := s.db.QueryRow("SELECT number, client, status, address, createdAt FROM parcel WHERE id = :number",
+		sql.Named("number", number))
 
 	// заполните объект Parcel данными из таблицы
 	p := Parcel{}
+
+	err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+	if err != nil {
+		// возвращаем частично заполненный Parsel или создать новый?
+		return p, err
+	}
 
 	return p, nil
 }
 
 func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
-	// реализуйте чтение строк из таблицы parcel по заданному client
-	// здесь из таблицы может вернуться несколько строк
-
+	rows, err := s.db.Query("SELECT number, client, status, address, createdAt FROM parcel WHERE client = :client",
+		sql.Named("client", client))
+	if err != nil {
+		return nil, err
+	}
 	// заполните срез Parcel данными из таблицы
 	var res []Parcel
 
+	for rows.Next() {
+		p := Parcel{}
+		err := rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, p)
+	}
 	return res, nil
 }
 
 func (s ParcelStore) SetStatus(number int, status string) error {
 	// реализуйте обновление статуса в таблице parcel
+	res, err := s.db.Exec("UPDATE parсel SET status = :status WHERE number = :number",
+		sql.Named("status", status),
+		sql.Named("number", number))
+
+	if err != nil {
+		return err
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	// не совсем понял, вернётся ли ошибка из Exec если он не повлияет ни на 1 строку, но лучше проверить:
+	if count == 0 {
+		return fmt.Errorf("SetStatus: не удалось выставить статус %s для посылки %d", status, number)
+	}
 
 	return nil
 }
 
 func (s ParcelStore) SetAddress(number int, address string) error {
-	// реализуйте обновление адреса в таблице parcel
-	// менять адрес можно только если значение статуса registered
+	// Есть 2 стула: делать SELECT для просмотра статуса записи в БД || делать UPDATE с жестким условием по статусу
+	// + меньше запросов к бд
+	// + между запросом статуса и выполнением UPDATE кто-то может статус изменить
+	// - нет явной ошибки в случае невыполнения UPDATE (запись не изменилась потому что невалидный ID || потому что статус не тот?)
+
+	res, err := s.db.Exec("UPDATE parсel SET address = :address WHERE number = :number AND status = :status",
+		sql.Named("number", number),
+		sql.Named("status", ParcelStatusRegistered))
+
+	// Аналогично, 	не совсем понял, вернётся ли ошибка из Exec если он не повлияет ни на 1 строку - в данном случае может быть критично.
+	if err != nil {
+		return err
+	}
+
+	count, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if count == 0 {
+		return fmt.Errorf("SetAddress: не удалось сменить адрес для посылки %d", number)
+	}
 
 	return nil
 }
@@ -64,6 +117,23 @@ func (s ParcelStore) SetAddress(number int, address string) error {
 func (s ParcelStore) Delete(number int) error {
 	// реализуйте удаление строки из таблицы parcel
 	// удалять строку можно только если значение статуса registered
+
+	res, err := s.db.Exec("DELETE FROM parсel WHERE number = :number AND status = :status",
+		sql.Named("number", number),
+		sql.Named("status", ParcelStatusRegistered))
+
+	if err != nil {
+		return err
+	}
+
+	count, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if count == 0 {
+		return fmt.Errorf("Delete: не удалось удалить посылку %d", number)
+	}
 
 	return nil
 }
